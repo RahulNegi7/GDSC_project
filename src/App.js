3700e3b940c2ab9d997977b2f697e557303632bf
Rahul Negi <rahulnegi0465@gmail.com>
2024-03-27 23:30:33 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Markdownreact from "./markdown/markdownreact";
+import './App.css';
+
+function App() {
+  return <Markdownreact />;
+}
+
+export default App;
---
+++ src/markdown/markdownreact.js
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+import JoditEditor from 'jodit-react';
+import './styles.css';
+
+const Markdown = () => {
+  const editor = useRef(null)
+  const [content,setContent] = useState('')
+  const [markDown,setMarkDown] = useState("Welcome");
+  return (
+    <>
+        <header>
+          <b>Markdown-Previewer</b>
+        </header>
+
+        <div className="center-div"> 
+
+        <div className='left-side'>
+        <b1 className="left-head">Type here</b1>
+          <textarea className="left-side" 
+          value={markDown} 
+          onChange={(e) => setMarkDown(e.target.value)}
+          >  
+          
+          
+          <JoditEditor
+			    ref={editor}
+			    value={content}
+			    onChange={newContent => setContent(newContent)}
+		      />
+          
+          </textarea>
+          </div>
+
+          <div className="right-side">
+          <b2 className="right-head">Preview</b2>
+            <ReactMarkdown>{markDown}</ReactMarkdown>
+          </div>
+
+        </div>
+    </>  
+  );
+};
+
+export default Markdown;
